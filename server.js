72b767a383d02283ade3bcec4de59b4838e84ee4
ryagasanjay <ryagasanjay@gmail.com>
2019-08-14 23:15:03 -0400

    Fixed API for the New Items addition, Added new Calls for getting the new APIs

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mysql = require('mysql')
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json({ type: 'application/json' }));
 app.use(express.static('./public'));
 
 
@@ -48,7 +49,7 @@ app.get('/users/:name', (req, res) => {
             res.sendStatus(500);
             return;
         }
-        res.json(rows);
+        res.json(rows[0]);
     });
 
     //res.end();
@@ -56,21 +57,34 @@ app.get('/users/:name', (req, res) => {
 
 app.post('/postCustomer', (req, res) => {
     console.log('Trying to create a new user');
-    console.log(req.body.age);
-    console.log(req.body.name);
-
+    console.log(req.body.uname);
+    console.log(req.body.psw);
+    var usernameparm = req.body.uname;
     const queryString = "INSERT INTO CUSTOMER (name, age) values(?,?)";
-    getConnection().query(queryString, [req.body.name, req.body.age], (err, result, fields) => {
+    getConnection().query(queryString, [req.body.uname, req.body.psw], (err, result, fields) => {
+            if (err) {
+                console.log('Failed to insert new user');
+                //res.sendStatus(500);
+                return res.json({
+                    errors: ['Failed to insert new user. Hey. Its nobodys fault']
+                });
+            }
+        })
+        //res.json(result.rows[0]);
+
+    const querystring = 'SELECT * FROM CUSTOMER WHERE name=?';
+    getConnection().query(querystring, usernameparm, (err, rows, fields) => {
         if (err) {
-            console.log('failed to insert new user');
+            console.log("Failed query for the transaction");
             res.sendStatus(500);
             return;
-
         }
-        console.log("Inserted a new user with name:", result.insertedId);
+        console.log("Failed query for the transaction", rows);
+        res.status(201).json(rows[0]);
         res.end();
-    })
-    res.end();
+    });
+
+
 });
 
 //http://localhost:3000/checkLogin?uname=sanjaysai999&psw=123456789
@@ -89,7 +103,7 @@ app.post('/checkLogin', function(req, res) {
             return
         }
         // sending all the selected rows as json to the front end as post response
-        res.json(rows);
+        res.status(200).json(rows);
         // console.log(rows[0]);
         // res.redirect('/about.html');
         res.end();
@@ -97,37 +111,67 @@ app.post('/checkLogin', function(req, res) {
     })
 });
 
-app.post('/InsertingIntoListofItems', function(req, res) {
 
-    const queryString = "INSERT INTO listofitems (`item_name`,`quantity`,`IsSelected`) VALUES (?,?,?);";
-    if (req, body.isSelected == 'true') {
-        var isSelected = 1;
-    } else {
-        var isSelected = 0;
+//Post Call for the new Items for a new customer
+app.post('/insertingIntoListofItems', function(req, res) {
+
+
+    // if (req.body.isSelected == 'true') {
+    //     var isSelected = 1;
+    // } else {
+    //     var isSelected = 0;
+    // }
+    // var customerID = 1;
+    var data = [
+        req.body.item_name,
+        req.body.quantity,
+        req.body.IsSelected,
+        req.body.CustomerID
+    ];
+    var custID = req.body.CustomerID;
+
+    console.log(custID);
+    for (var i = 0; i < data.length; i++) {
+        console.log(data[i]);
     }
-    getConnection().query(queryString, [req.body.item_name, req.body.quantity, isSelected], (err, result, fields) => {
+    const insertqueryStr = "INSERT INTO listofitems (item_name, quantity, IsSelected, CustomerID) VALUES (?,?,?,?)";
+    getConnection().query(insertqueryStr, data, (err, result, fields) => {
         if (err) {
-            console.log('failed to insert new user');
+            console.log('Failed to insert new user', err);
+
+            res.status(500).json({
+                errors: ['Failed to insert new user. Hey. Its nobodys fault']
+            });
+        }
+        console.log(result);
+    });
+
+    const querystring = 'SELECT * FROM listofitems WHERE CustomerID=? ';
+    getConnection().query(querystring, custID, (err, rows, fields) => {
+        if (err) {
+            console.log("Failed query for the transaction", err);
             res.sendStatus(500);
             return;
-
         }
-        console.log("Inserted a new user with name:", result.insertedId);
+        console.log("Failed query for the transaction", rows);
+        res.status(201).json(rows);
         res.end();
-    })
-    res.end();
+    });
+    // //res.end();
 });
 
-app.get('/getMyItems', (req, res) => {
-    // var user1 = { name: "sanjay", age: '25' };
-    // var user2 = { name: "jeevan", age: '22' };
-    // var user3 = { name: "jeevanp", age: '22' };
-
-    // res.json([user1, user2]);
-    const getUserQuery = "SELECT * FROM CUSTOMER";
-
-    getConnection().query(getUserQuery, (err, rows, fields) => {
-            res.json(rows);
-        })
-        // res.send("nodemon auto updates the server again now");
+//to Get all items for the customer.
+app.get('/getMyItems/:custID', (req, res) => {
+    var customerID = req.params.custID;
+    const querystring = 'SELECT * FROM listofitems WHERE CustomerID=? ';
+    getConnection().query(querystring, customerID, (err, rows, fields) => {
+        if (err) {
+            console.log("Failed query for the transaction", err);
+            res.sendStatus(500);
+            return;
+        }
+        console.log("Failed query for the transaction", rows);
+        res.status(201).json(rows);
+        res.end();
+    });
 });
